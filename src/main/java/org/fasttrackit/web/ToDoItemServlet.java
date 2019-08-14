f50fc34c921261ff5fa3cb73b744e5a2a93bbd53
ioana.cora <ioana.cora@casomes.ro>
2019-08-14 21:13:51 +0300

    added delete toDoItems

--- src/main/java/org/fasttrackit/web/ToDoItemServlet.java
+++ src/main/java/org/fasttrackit/web/ToDoItemServlet.java
@@ -53,4 +53,15 @@ public class ToDoItemServlet extends HttpServlet {
             resp.sendError(500,"Internal Server Error " + e.getMessage());
         }
     }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String id= req.getParameter ( "id");
+
+        try {
+            toDoItemService.deleteToDoItem(Long.parseLong(id));
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500,"Internal Server Error " + e.getMessage());
+        }
+    }
 }
